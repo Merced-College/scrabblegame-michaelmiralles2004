b6b5218957ebd06f9a8b73a11958e213437fa51d
Michael Miralles <149123710+michaelmiralles2004@users.noreply.github.com>
2025-06-29 03:13:22 +0000

    Finished skeleton for ScrabbleGame class

---
+++ ScrabbleGame.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ScrabbleGame {
+    private static ArrayList<Word> listOfWords = new ArrayList <>();
+    private static final Random rand = new Random();
+    private static final Scanner scnr = new Scanner(System.in);
+
+    private static void main (String[] args) {}
+
+    private static void introduction() {}
+
+    private static void loadWords() {}
+
+    public static char[] letterGenerator() {}
+
+    public static boolean isWord() {}
+
+    public static boolean isCorrectWord() {}
+}
--- Word.java
+++ Word.java
@@ -1,19 +1,23 @@
 public class Word implements Comparable<Word> {
     private String word;
 
+    // Constructor: store word in lowercase form
     public Word (String word) {
         this.word = word.toLowerCase();
     }
 
+    // Get word function
     public String getWord () {
         return word;
     }
 
+    // Compare function: Compares two Word objects
     @Override
     public int compareTo(Word other) {
         return this.word.compareTo(other.word);
     }
 
+    // Printing a word
     @Override
     public String toString() {
         return word;
